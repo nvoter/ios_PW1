365c5ca50f789b7fd534efe37c92c73e8cf978ea
Anastasia Manushkina <avmanushkina@edu.hse.ru>
2023-09-15 17:33:43 +0300

    Initial Commit

---
+++ avmanushkinaPW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avmanushkinaPW1
+//
+//  Created by Анастасия on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
